97b2c4904dba951b8c2a91867ab0ede54803767f
mmdcloud <madmaxcloudpractice@gmail.com>
2025-06-15 10:11:39 +0530

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+# EC2 IAM Instance Profile
+data "aws_iam_policy_document" "instance_profile_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "instance_profile_iam_role" {
+  name               = "instance-profile-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.instance_profile_assume_role.json
+}
+
+data "aws_iam_policy_document" "instance_profile_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:*"]
+    resources = ["*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "instance_profile_s3_policy" {
+  role   = aws_iam_role.instance_profile_iam_role.name
+  policy = data.aws_iam_policy_document.instance_profile_policy_document.json
+}
+
+resource "aws_iam_instance_profile" "iam_instance_profile" {
+  name = "iam-instance-profile"
+  role = aws_iam_role.instance_profile_iam_role.name
+}
+
+module "frontend_instance" {
+  source                      = "./modules/ec2"
+  name                        = "frontend-instance"
+  ami_id                      = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  key_name                    = "madmaxkeypair"
+  associate_public_ip_address = true
+  user_data                   = filebase64("${path.module}/scripts/user_data.sh")
+  instance_profile            = aws_iam_instance_profile.iam_instance_profile.name
+  subnet_id                   = module.public_subnets.subnets[0].id
+  security_groups             = [module.security_group.id]
+}
